a202829401add170123b71e18d29607eb44a8991
Pritesh Golakiya <priteshgolakiya1@gmail.com>
2024-04-10 15:54:19 +0530

    Complet CRUD

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const connect = require("./db/connection");
 const userRouter = require("./routes/userRoutes");
+const errorMiddleware = require("./middlewares/errorMiddleware");
 require("dotenv").config();
 
 const app = express();
@@ -16,22 +17,18 @@ app.use(bodyParser.json());
 app.use("/user", userRouter);
 
 // Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
-});
+app.use(errorMiddleware);
 
-// Start server and database connection
 const start = async () => {
   try {
-    await connect(); // Assuming this function returns a promise for database connection
+    await connect();
     app.listen(PORT, () => {
       console.log("Connected successfully to the database!");
       console.log(`Server is listening on port ${PORT}!`);
     });
   } catch (error) {
     console.error("Failed to connect to the database:", error);
-    process.exit(1); // Exit the process with a non-zero code indicating failure
+    process.exit(1);
   }
 };
 
